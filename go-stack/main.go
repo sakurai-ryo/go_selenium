1b5fb8858a041f27fae00bf96c76e45c1f6c7d2d
sakurai-ryo <sakurairyo.10do@gmail.com>
2021-01-26 18:47:23 +0900

    update: add sample scraping code

--- go-stack/main.go
+++ go-stack/main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/sclevine/agouti"
 )
 
 func main() {
@@ -15,8 +17,34 @@ func handler(ctx context.Context, request events.APIGatewayProxyRequest) (events
 	log.Println("ctx: ", ctx)
 	log.Println("request: ", request)
 
+	title, err := scraping()
+	if err != nil {
+		return events.APIGatewayProxyResponse{
+			Body:       "failed",
+			StatusCode: 400,
+		}, err
+	}
+
 	return events.APIGatewayProxyResponse{
-		Body:       "success",
+		Body:       fmt.Sprintf("get Title: %s", title),
 		StatusCode: 200,
 	}, nil
+}
+
+func scraping() (string, error) {
+	driver := agouti.ChromeDriver()
+	defer driver.Stop()
+
+	if err := driver.Start(); err != nil {
+		return "", fmt.Errorf("failed to start driver: %s", err)
+	}
+
+	page, err := driver.NewPage()
+	log.Println("new Pages: ", page)
+	if err != nil {
+		return "", fmt.Errorf("failed to create NewPages: %s", err)
+	}
+
+	page.Navigate("https://www.google.com/")
+	return page.Title()
 }
